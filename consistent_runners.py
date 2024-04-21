dc6a1a453a67d038743327c7e8f36ba336803866
Alex <alex.saltiel@gmail.com>
2024-04-21 15:33:13 -0400

    Update to the consistent runner script to make it more accurate and include commentted out code for graphs and fixed diff logic.

--- consistent_runners.py
+++ consistent_runners.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
@@ -8,16 +9,14 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
 
 #%% Read and separate data
 #d_data = pd.read_parquet('dataset/run_ww_2019_d.parquet', engine='pyarrow')
 w_data = pd.read_parquet('dataset/run_ww_2019_w.parquet', engine='pyarrow')
-#m_data = pd.read_parquet('dataset/run_ww_2019_m.parquet', engine='pyarrow')
+m_data = pd.read_parquet('dataset/run_ww_2019_m.parquet', engine='pyarrow')
 
 # separate training data from test data
-# 5537 monthly runners, take 1250 for training, 1250 for validation and rest for test
+# 5537 weekly runners, take 1250 for training, 1250 for validation and rest for test
 # pruning all datasets to weekly runners
 iw = w_data[w_data.distance>0].athlete.value_counts()
 weekly_runners = iw.index[iw.gt(51)].tolist()
@@ -28,6 +27,19 @@ train_idx, val_idx = train_test_split(train_idx, train_size=1250)
 x_train = w_data[w_data['athlete'].isin(train_idx)]
 x_val = w_data[w_data['athlete'].isin(val_idx)]
 x_test = w_data[~w_data['athlete'].isin(val_idx) & ~w_data['athlete'].isin(train_idx)]
+
+# separate training data from test data
+# 23027 monthly runners, take 6000 for training, 6000 for validation and rest for test
+# pruning all datasets to monthly runners
+# im = m_data[m_data.distance>0].athlete.value_counts()
+# monthly_runners = im.index[im.gt(11)].tolist()
+
+# train_idx, test_idx = train_test_split(monthly_runners, train_size=12000)
+# train_idx, val_idx = train_test_split(train_idx, train_size=6000)
+        
+# x_train = m_data[m_data['athlete'].isin(train_idx)]
+# x_val = m_data[m_data['athlete'].isin(val_idx)]
+# x_test = m_data[~m_data['athlete'].isin(val_idx) & ~m_data['athlete'].isin(train_idx)]
 
 #%% Generate new features in training and validation set from day data
 for trainValn in range(2):
@@ -65,12 +77,10 @@ for trainValn in range(2):
             week_idx = ath_data.index[pd.to_datetime(ath_data.datetime).dt.isocalendar().week % 4 == j]
             week_ath_data = ath_data[ath_data.index.isin(week_idx)]
 
-            dist_diff = abs(ath_data.distance.diff()).tolist()
+            dist_diff = week_ath_data.distance.diff().tolist()
             dist_diff[0] = 0
-            dur_diff = abs(ath_data.duration.diff()).tolist()
+            dur_diff = week_ath_data.duration.diff().tolist()
             dur_diff[0] = 0
-            pace_diff = abs(pace.diff()).tolist()
-            pace_diff[0] = 0
             
             s = 'week_' + str(j) + '_'
             
@@ -78,8 +88,6 @@ for trainValn in range(2):
             new_entry[s + 'distance_diff_variance'] = np.var(dist_diff)
             new_entry[s + 'duration_diff_avg'] = np.average(dur_diff)
             new_entry[s + 'duration_diff_variance'] = np.var(dur_diff)
-            new_entry[s + 'pace_diff_avg'] = np.average(pace_diff)
-            new_entry[s + 'pace_diff_variance'] = np.var(pace_diff)
             
         enum_set.append(new_entry)
         
@@ -101,20 +109,41 @@ for trainValn in range(2):
         enum_val_df_norm[enum_df_num.columns] = scaler.fit_transform(enum_df_num)
 
 
+# Used to create graphs
+# enum_train_df = pd.DataFrame(enum_set)
+# female_data = enum_train_df[enum_train_df['gender']==0]
+# male_data = enum_train_df[enum_train_df['gender']==1]
+# xs = [0, 1, 2, 3]
+# labels = ['Week 1', 'Week 2', 'Week 3', 'Week 4']
+# plt.figure(1)
+# m_dist_data =[
+#     np.average(male_data['week_0_distance_diff_variance']),
+#     np.average(male_data['week_1_distance_diff_variance']),
+#     np.average(male_data['week_2_distance_diff_variance']),
+#     np.average(male_data['week_3_distance_diff_variance']),
+#     ]
+# f_dist_data = [
+#     np.average(female_data['week_0_distance_diff_variance']),
+#     np.average(female_data['week_1_distance_diff_variance']),
+#     np.average(female_data['week_2_distance_diff_variance']),
+#     np.average(female_data['week_3_distance_diff_variance']),
+#     ]
+# plt.xticks(xs, labels)
+# #plt.ylabel('Kilometers')
+# plt.plot( xs, m_dist_data, color='blue', label='Men')
+# plt.plot( xs, f_dist_data, color='red', label='Women')
+# plt.title('Variance of Weekly Change in Running Distance')
+# plt.legend(loc='upper right')
+# plt.grid(True)
+# plt.show()
+
+
 #%% logistic regression
 y_train = enum_train_df[['gender']]
 y_val = enum_val_df[['gender']]
 
 x_vals = enum_train_df_norm.drop(['athlete', 'gender'], axis=1).values
 y_vals = y_train.values
-
-selector = SelectKBest(score_func=f_classif, k=4)
-xfs = selector.fit_transform(x_vals, y_vals)
-selected_indices = selector.get_support(indices=True)
-selected_feature_names = enum_train_df_norm.drop(['athlete', 'gender'], axis=1).columns[selected_indices]
-
-enum_train_df = enum_train_df[selected_feature_names]
-enum_val_df = enum_val_df[selected_feature_names]
 
 # Fit the model with the training data
 logistic_model = LogisticRegression(class_weight={0:0.7, 1:0.3})#, solver="liblinear")
